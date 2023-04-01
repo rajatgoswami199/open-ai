a6b44988055f7b6c30a5f154473811012fcdc012
Rajat Goswami <rajatgoswami@Rajats-Mac-mini.local>
2023-04-02 01:01:37 +0530

    change server url to live

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5005/', {
+    const response = await fetch('https://opencodex.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
